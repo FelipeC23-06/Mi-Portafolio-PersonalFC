c08d33c2fe62a6eaac378dec4bf0e9147750d78f
FelipeC23-06 <luisfecabrales06@gmail.com>
2025-02-18 11:09:33 +0100

    Update acciones_javascript.js

--- acciones_javascript.js
+++ acciones_javascript.js
@@ -1,1 +1,7 @@
+console.log("Bienvenido al portafolio de [Tu Nombre y Apellido]");
 
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function() {
+        alert('Abriendo el proyecto: ' + this.href);
+    });
+});
